7b89f9addeacd0371accbfc2f0b8d6b81522add6
Tour4X <tour4x@qq.com>
2016-03-23 18:22:50 +0800

    add layout

--- learnColletionView/RACollectionViewReorderTripletLayout/RACollectionVIewDelegateTripleLayout.h
+++ learnColletionView/RACollectionViewReorderTripletLayout/RACollectionVIewDelegateTripleLayout.h
@@ -8,6 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
+#define RACollectionVIewTripletLayoutStyleSquare CGSizeZero
+
+@protocol RACollectionViewDelegateTripletLayout <UICollectionViewDelegateFlowLayout>
+
+@optional
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForLargeItemsInSection:(NSInteger)section;//Default to automaticaly grow square !
+
+@end
+
 @interface RACollectionVIewDelegateTripleLayout : UICollectionViewLayout
+
+@property (assign, nonatomic)id<RACollectionViewDelegateTripletLayout> delegate;
+@property (assign, nonatomic, readonly)CGSize largeCellSize;
+@property (assign, nonatomic, readonly)CGSize smallCellSize;
 
 @end
