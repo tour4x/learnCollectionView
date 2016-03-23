7c856510ec7f8e7a3f1227412c9e8df2fc8719d2
Tour4X <tour4x@qq.com>
2016-03-23 14:56:56 +0800

    add layout

---
+++ learnColletionView/RACollectionViewReorderTripletLayout/RACollectionVIewDelegateTripleLayout.h
@@ -0,0 +1,13 @@
+//
+//  RACollectionVIewDelegateTripleLayout.h
+//  learnColletionView
+//
+//  Created by user on 16/3/23.
+//  Copyright © 2016年 JiuFang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RACollectionVIewDelegateTripleLayout : UICollectionViewLayout
+
+@end
